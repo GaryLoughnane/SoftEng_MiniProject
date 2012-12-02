435e07aa8ac3df2fecd2316f5e1f048fc9813e43
Gary Loughnane <mr.brownstone1987@hotmail.com>
2012-12-02 21:23:41 +0000

    started building GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,37 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import net.miginfocom.swing.MigLayout;
+
 
 public class GUI {
+	
+	public GUI(){
+		
+		
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new MigLayout("", "[grow]", "[grow][grow]"));
+		
+		JTabbedPane inputPanel = new JTabbedPane();
+		
+		JPanel outputPanel = new JPanel();
+		
+		mainPanel.add(inputPanel);
+		mainPanel.add(outputPanel);
+		
+		
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(mainPanel);
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+		
+		
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		new GUI();
 
 	}
 
