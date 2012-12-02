486eb7259cac86b2a5548becd8e67e3553c0f647
Gary Loughnane <mr.brownstone1987@hotmail.com>
2012-12-02 22:12:36 +0000

    system look and feel, some tabs, icon, full screen

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,10 @@
+
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -9,24 +13,40 @@ public class GUI {
 	
 	public GUI(){
 		
+		//set look and feel to "system look and feel"
+		try{UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}
+		catch(Exception e){
+			//TODO add exception code later
+		}
 		
 		
+		//create main "background" panel
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new MigLayout("", "[grow]", "[grow][grow]"));
 		
-		JTabbedPane inputPanel = new JTabbedPane();
+		//create each of the "question type panels
+		JPanel trueFalse = new JPanel();
+		JPanel multiChoice = new JPanel();
 		
+		//create tabbed panel and add each question type panel to it
+		JTabbedPane inputPanel = new JTabbedPane(JTabbedPane.TOP);
+		inputPanel.addTab("True/False", trueFalse);
+		inputPanel.addTab("Multiple Choice", multiChoice);
+		
+		//create the output panel
 		JPanel outputPanel = new JPanel();
 		
-		mainPanel.add(inputPanel);
-		mainPanel.add(outputPanel);
+		//add input (tabbed) and output panels to main panel
+		mainPanel.add(inputPanel, "cell 0 0,grow");
+		mainPanel.add(outputPanel, "cell 0 1,grow");
 		
-		
-		
-		JFrame frame = new JFrame();
+		//setup JFrame
+		//Frame text, logo, close operation, add main panel, set full screen, show
+		JFrame frame = new JFrame("Offline GIFT Editor - By Gary Loughnane");	
+		frame.setIconImage(new ImageIcon(GUI.class.getResource("images/moodle.gif")).getImage());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(mainPanel);
-		frame.pack();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setVisible(true);
 		
 		
