f6fa50adff209b3742318c0d4a47a328f1b1daa7
Gary Loughnane <mr.brownstone1987@hotmail.com>
2012-12-03 20:33:16 +0000

    added multi choice tab with option to add any amount of possible answers and select one correct one

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,10 @@
 
-import java.awt.Color;
+
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -43,16 +46,17 @@ public class GUI {
 		
 		//create each of the "question type panels
 		JPanel trueFalse = new JPanel();
-		JPanel multiChoice = new JPanel();
+		JPanel multiChoice1 = new JPanel();
 		
 		
 		setupTrueFalse(trueFalse);
+		setupMultiChoice1(multiChoice1);
 		
 		
 		//create tabbed panel and add each question type panel to it
 		JTabbedPane inputPanel = new JTabbedPane(JTabbedPane.TOP);
 		inputPanel.addTab("True/False", trueFalse);
-		inputPanel.addTab("Multiple Choice", multiChoice);
+		inputPanel.addTab("Multiple Choice", multiChoice1);
 		
 		
 		//create the output panel
@@ -75,6 +79,116 @@ public class GUI {
 		frame.setVisible(true);
 
 		
+	}
+
+	private void setupMultiChoice1(JPanel panel) {
+				
+		panel.setLayout(new MigLayout("", "[grow,left][grow]", "[][][][grow][]"));
+		
+		JLabel explination = new JLabel("Multiple choice question with one correct answer");
+		JLabel title = new JLabel("Question Title:");	
+		JLabel prompt = new JLabel("Enter a question.");
+		JLabel selectPrompt = new JLabel("Enter all possible answers and select one correct answer.");
+		
+		final JTextField titleName = new JTextField();	
+		
+		final JTextArea question = new JTextArea();	
+		JScrollPane questionScroll = new JScrollPane(question);
+		
+		final JPanel answerPanel = new JPanel();
+		answerPanel.setLayout(new MigLayout("", "[grow,fill][right]", "[][]"));	
+		JScrollPane answerScroll = new JScrollPane(answerPanel);
+		
+		JButton addAnswer = new JButton("Add Answer");
+		JButton remAnswer = new JButton("Remove Answer");
+		JButton generate = new JButton("Generate Code");	
+		JButton clear = new JButton("Clear");
+		
+		final ButtonGroup selectgrGroup = new ButtonGroup();
+		final ArrayList<JTextField> answers = new ArrayList<JTextField>();
+		final ArrayList<JRadioButton> select = new ArrayList<JRadioButton>();
+		
+		for(int i=0; i<2; i++){
+			answers.add(new JTextField());
+			select.add(new JRadioButton());
+			
+			selectgrGroup.add(select.get(i));
+		}
+		
+		
+		addAnswer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				answers.add(new JTextField());
+				select.add(new JRadioButton());
+				
+				answerPanel.add(answers.get(answers.size()-1), "flowy,cell 0 1,growx");
+				answerPanel.add(select.get(select.size()-1), "flowy,cell 1 1,alignx right");
+				
+				selectgrGroup.add(select.get(select.size()-1));
+				
+				answerPanel.updateUI();
+				
+			}
+		});
+		
+		
+		remAnswer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(answers.size()>2){
+				
+					answerPanel.remove(answers.get(answers.size()-1));
+					answerPanel.remove(select.get(select.size()-1));
+					
+					selectgrGroup.remove(select.get(select.size()-1));
+					
+					answers.remove(answers.size()-1);
+					select.remove(select.size()-1);
+					
+					answerPanel.updateUI();
+				}
+			}
+		});
+		
+		
+		clear.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				titleName.setText("");
+				question.setText("");
+				
+				for(int i=0; i<answers.size(); i++){
+					answers.get(i).setText("");
+				}
+				
+			}
+		});
+		
+		for(int i=0; i<2; i++){
+			answerPanel.add(answers.get(i), "flowy,cell 0 1,growx");
+			answerPanel.add(select.get(i), "flowy,cell 1 1,alignx right");
+		}
+				
+		panel.add(explination, "cell 0 0");
+		panel.add(title, "flowx,cell 0 1,alignx left");
+		panel.add(titleName, "cell 0 1,alignx left, grow");
+		panel.add(prompt, "cell 0 2");
+		panel.add(questionScroll, "cell 0 3,grow");
+		
+		
+		panel.add(selectPrompt, "align right, cell 1 2");
+		panel.add(addAnswer, "align right, cell 1 2");	
+		panel.add(remAnswer, "align right, cell 1 2");	
+		panel.add(answerScroll, "cell 1 3, grow");
+		panel.add(clear, "align right, cell 1 4");
+		panel.add(generate, "align right, cell 1 4");
+	
 	}
 
 	private void setupOutput(JPanel panel) {
